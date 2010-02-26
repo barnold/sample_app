4e34be76c0b5cbca962538dafbd8bcd1c63edb18
Ben Arnold <benarnold@ntlworld.com>
2010-02-26 08:46:59 +0000

    saving users

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -42,4 +42,42 @@ describe UsersController do
 		end
 	end
 	
+	describe "POST 'create'" do
+		describe "failure" do
+			it "should render the 'new' page" do
+				post :create, :user => {}
+				response.should render_template('new')
+			end
+			
+			it "should have the right titlle" do
+				post :create, :user => {}
+				response.should have_tag("title", /sign up/i)
+			end
+		end
+		
+		describe "success" do
+			before(:each) do
+				@attr = { :name => "New User", :email => "user@example.com",
+					:password => "foobar", :password_confirmation => "foobar" }
+				@user = Factory.build(:user, @attr)
+				User.stub!(:new).and_return(@user)
+			end
+			
+			it "should save the new user" do
+				@user.should_receive(:save).and_return(true)
+				post :create, :user => @attr
+			end
+			
+			it "should redirect to the user show page" do
+				post :create, :user => @attr
+				response.should redirect_to(user_url(@user))
+			end
+			
+			it "should have a welcome message" do
+				post :create, :user => @attr
+				flash[:success].should =~ /welcome to the sample app/i
+			end
+		end
+	end
+	
 end
